42163841d0715e33723dcde7315533a943cb0492
Alan <alannkgw@hotmail.com>
2025-01-17 01:40:07 -0300

    readme add

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,57 @@
+import React, { useRef } from 'react'
+
+
+const Navbar = () => {
+
+    const lastActiveLink = useRef<HTMLAnchorElement | null>(null);
+    const activeBox = useRef<HTMLDivElement | null>(null);
+
+    const navItems = [
+        {
+            label: 'Home',
+            link: '#home',
+            className: 'nav-link active',
+            ref: lastActiveLink
+        },
+        {
+            label: 'About',
+            link: '#about',
+            className: 'nav-link'
+        },
+        {
+            label: 'Work',
+            link: '#work',
+            className: 'nav-link'
+        },
+        {
+            label: 'Reviews',
+            link: '#reviews',
+            className: 'nav-link'
+        },
+        {
+            label: 'Contact',
+            link: '#contact',
+            className: 'nav-link md:hidden'
+        }
+    ];
+    return (
+        <nav className='navbar'>
+            {
+                navItems.map(({ label, link, className, ref }, key) => (
+                    <a href={link}
+                        key={key}
+                        ref={ref}
+                        className={className}
+                        onClick={() => { }}
+                    >
+                        {label}
+                    </a>
+                ))
+            }
+            <div className='active-box' 
+            ref={activeBox}></div>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+/*Node modules*/ 
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+/*Components*/ 
+import App from './App.tsx'
+
+/* CSS */
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
